11325248295dbfe7ed877830734bfe13b3f32c2c
jpw <jon@mdi.eu.com>
2019-02-04 10:40:34 +0000

    reads and outputs a local file

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const fs = require('fs').promises;
+const path = require('path');
+const express = require('express');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const app = express();
+const port = 3000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const docsPath = path.join(__dirname, '../', 'trop-docs/DOCS.md')
 
+app.get('/', async (req, res, next) => {
+    try {
+        const content = await fs.readFile(docsPath, 'utf-8');
+        res.send(content);
+    } catch (err) {
+        next(err);
+    }
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
