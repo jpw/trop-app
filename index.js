cf8c1e2809b78fdf0de7848f4cdbb1e9b28d4ad9
jpw <jon@mdi.eu.com>
2019-02-13 18:12:55 +0100

    nicer error handling

--- index.js
+++ index.js
@@ -5,21 +5,26 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-const docsPath = path.join(__dirname, '../', 'trop-docs/DOCS.md')
+const docsPath = path.join(__dirname, '../', 'trop-docs/DOCSs.md')
 
 const getFileContent = async () =>  {
     let content;
     try {
         content = await fs.readFile(docsPath, 'utf-8');
     } catch (err) {
-        console.error(err);
+        content = err;
     }
-
     return content;
 };
 
 app.get('/', async (req, res, next) => {
-    res.send(await getFileContent());
+    const readResult = await getFileContent();
+    if (readResult instanceof Error) {
+        next(readResult)
+    }
+    else {
+        res.send(readResult);
+    }
 })
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
